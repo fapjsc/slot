799ee62b8423431f1730f0027b9d39dd8488d800
Mouse <dino11070808@gmail.com>
2021-05-24 15:33:47 +0800

    [Added by Dino] Upload web code.

---
+++ slot-web/src/App.js
@@ -0,0 +1,39 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default App;
