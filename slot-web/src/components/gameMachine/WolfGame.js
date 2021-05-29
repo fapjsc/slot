e38ad002a0689662c2f969f9aa15049c6946245e
fapjsc <p82szh@gmail.com>
2021-05-29 18:11:43 +0800

    create new machine => wolf

--- slot-web/src/components/gameMachine/WolfGame.js
+++ slot-web/src/components/gameMachine/WolfGame.js
@@ -1,25 +1,24 @@
 import { useHistory } from 'react-router-dom';
 
 // Style
-import machineIcon from '../../asset/egzj1ui37v.jpeg';
 import { makeStyles } from '@material-ui/core/styles';
-// import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-// import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
-// import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from '@material-ui/core/Button';
+
+// Image
+import machineIcon from '../../asset/egzj1ui37v.jpeg';
+import wolfImg from '../../asset/wolf.png';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 250,
+    width: 250,
     backgroundColor: '#f2f2f2',
   },
   media: {
@@ -45,11 +44,6 @@ export default function MachineItem() {
   const classes = useStyles();
 
   let history = useHistory();
-  //   const [expanded, setExpanded] = useState(false);
-
-  //   const handleExpandClick = () => {
-  //     setExpanded(!expanded);
-  //   };
 
   const selectMachine = () => {
     history.replace('/gamestart');
@@ -60,46 +54,26 @@ export default function MachineItem() {
       <CardHeader
         avatar={
           <Avatar aria-label="recipe" className={classes.avatar}>
-            X2
+            X4
           </Avatar>
         }
-        // action={
-        //   <IconButton aria-label="settings">
-        //     <MoreVertIcon />
-        //   </IconButton>
-        // }
-        title="麻雀物語2"
+        title="ARUZE雄狼"
         subheader="September 14, 2021"
       />
-      <CardMedia className={classes.media} image={machineIcon} title="Paella dish" />
+      <CardMedia className={classes.media} image={wolfImg} title="Paella dish" />
       <CardContent style={{ height: 90 }}>
         <Typography variant="body2" color="textSecondary" component="p">
-          機種類型：ART TYPE（含擬似BONUS）、純增2.8枚/G 50枚約可遊技轉數：32G
+          機種類型：美國原裝進口
+          <br />
+          最大押注:Mexbet 500
+          {/* 機種類型：ART TYPE（含擬似BONUS）、純增2.8枚/G 50枚約可遊技轉數：32G */}
         </Typography>
       </CardContent>
       <CardActions disableSpacing style={{ justifyContent: 'center' }}>
-        <Button variant="contained" color="primary" style={{}} onClick={() => selectMachine()}>
+        <Button variant="contained" color="primary" onClick={() => selectMachine()}>
           開始玩
         </Button>
-        {/* <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton> */}
       </CardActions>
-      {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>遊戲總數：2750</Typography>
-          <Typography paragraph>遊戲數：174</Typography>
-          <Typography paragraph>BIG：9</Typography>
-          <Typography>REG：15</Typography>
-        </CardContent>
-      </Collapse> */}
     </Card>
   );
 }
