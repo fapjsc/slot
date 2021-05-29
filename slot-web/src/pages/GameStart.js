6292872600bf20ca623756e2eb376d56abe4f133
XiangDennis <tim23959211@gmail.com>
2021-05-29 18:49:39 +0800

    更改樣式 新增左側欄 by Denny

--- slot-web/src/pages/GameStart.js
+++ slot-web/src/pages/GameStart.js
@@ -1,28 +1,101 @@
 import "./GameStart.css";
-import { Button } from "antd";
-import Layout from "antd/lib/layout/layout";
+import React, { useState } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+});
 
 const GameStart = () => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      dss
+    </div>
+  );
+
   return (
-    <Layout>
+    <>
+      <div>
+      {[ 'left' ].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+        ))}
+      </div>
       <div className="divPosition">
         <img className="backImg" src={"/barLeft.jpg"} />
       </div>
       <div className="divPosition">
         <img className="backImg" src={"/center.jpg"} />
-        <Button className="buttonPosition">MAX BET</Button>
+        <button className="buttonPosition" type="primary">
+          MAX BET
+        </button>
+        <button className="buttonPosition2" type="primary">
+          結算
+        </button>
+        <button className="buttonPosition3" type="primary">
+          SPIN
+        </button>
+        <button className="buttonPosition4" type="primary">
+          AUTO SPIN
+        </button>
+        <div>
+          <div className="gameTab">
+            <span
+              style={{
+                fontSize: "1.0rem",
+                color: "white",
+                fontWeight: "bold",
+                padding: "5px",
+              }}
+            >
+              點數
+            </span>
+            <input />
+            <button className="gameTabButton" type="primary">
+              投入代幣
+            </button>
+          </div>
+        </div>
       </div>
       <div className="divPosition">
         <img className="backImg" src={"/barRight.jpg"} />
       </div>
-
-      <div>
-        {/* <Button className="buttonPosition2" type="primary">
-            ssscccccs
-          </Button> */}
-      </div>
-      <Button type="text">ssss</Button>
-    </Layout>
+    </>
   );
 };
 
