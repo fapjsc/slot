e4be77d0cc56a516fdfdc62604e4bbf3d8b2c005
Coffee <kbb37038106@gmail.com>
2021-05-31 21:13:06 +0800

    udpate socket server and camera server

--- webrtc-socket-server/index.js
+++ webrtc-socket-server/index.js
@@ -1,45 +1,20 @@
 'use strict';
 
-// var os = require('os');
-// var nodeStatic = require('node-static');
 var fs = require('fs');
 var cors = require('cors');
 var express = require("express");
 var app = express();
 app.use(cors());
-// app.use(index);
 
 app.use("/", express.static(__dirname + "/"));
-// var options = {
-//   key: fs.readFileSync('./server.key'),
-//   ca: [fs.readFileSync('./server.csr')],
-//   cert: fs.readFileSync('./server.crt')
-// };
+
 var server = require("http").Server(app);
 
 var socketIO = require('socket.io')
 server.listen(3333);
-// app.get('/', function(req, res) {
-//     res.sendfile(__dirname + '/index.html');
-// });
-// app.get('/js/main.js', function(req, res) {
-//     res.sendfile(__dirname + '/js/main.js');
-// });
-// app.get('/js/main.js', function(req, res) {
-//     res.sendfile(__dirname + '/js/main.js');
-// });
-// var options = {
-//   key: fs.readFileSync('./server.key'),
-//   ca: [fs.readFileSync('./server.csr')],
-//   cert: fs.readFileSync('./server.crt')
-// };
-// var options = {}; 
-// var app = http.createServer(options, function (req, res) {
-//   // res.end('hello world\n');  
-//     fileServer.serve(req, res);
-// }).listen(3443);
-var io = socketIO.listen(server);
 
+var io = socketIO.listen(server);
+var device_map = [];
 io.on('connection', function(socket) {
   console.log("connected...");
   socket.on("hello",(message) => {
@@ -55,53 +30,64 @@ io.on('connection', function(socket) {
   }
 
   socket.on('message', function(message, room) {
+
+    if(!isNaN(parseInt(room))) {
+      var room = device_map[parseInt(room)];
+    }
+    else {
+      var room = room_map[room];
+    }
     log('Client said: ', message, room);
     // for a real app, would be room-only (not broadcast)
 
     socket.broadcast.emit('message', message, room);
   });
 
-  socket.on('create or join', function(room) {
+  socket.on('create', (device) => {
+      var clientsInRoom = io.sockets.adapter.rooms[device];
+      var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+      log('Room ' + device + ' now has ' + numClients + ' client(s)');
+      socket.join(device);
+      log('Client ID ' + socket.id + ' created room ' + device);
+      socket.emit('created', device);
+      device_map.push(device);
+      room_map[device] = device_map.length -1;
+  });
+
+  socket.on('join', function(room) {
     log('Received request to create or join room ' + room);
-
-    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var device_id = device_map[parseInt(room)];
+    var clientsInRoom = io.sockets.adapter.rooms[device_id];
     var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
-
-    if (numClients === 0) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room);
-
-    } else if (numClients === 1) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
+    log('Room ' + device_id + ' now has ' + numClients + ' client(s)');
+    if (numClients === 1) {
+      log('Client ID ' + socket.id + ' joined room ' + device_id);
+      io.sockets.in(device_id).emit('join', device_id);
+      socket.join(device_id);
       socket.emit('joined', room);
-    } else { // max two clients
+    } 
+    else if(numClients > 1 ){ // max two clients
       socket.emit('full', room);
     }
   });
-  
-  socket.on('ipaddr', function() {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
-        }
-      });
-    }
+  socket.on("getDeviceId",(room) => {
+    socket.emit("getDeviceId", device_map[parseInt(room)]);
   });
 
   socket.on('bye', function(){
+    device_map.length = 0;
     console.log('received bye');
+    socket.broadcast.emit('bye');
   });
   socket.on('leave', function(room){
     socket.leave(room);
     console.log("leave room");
   })
-  socket.on("check to connect" , function(){
-    socket.in(room).emit("check to connect" , numClients);
-  })
 });
+
+const printClientNum = () => {
+      var device_id = device_map[parseInt(room)];
+    var clientsInRoom = io.sockets.adapter.rooms[device_id];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    log('Room ' + device_id + ' now has ' + numClients + ' client(s)');
+}
