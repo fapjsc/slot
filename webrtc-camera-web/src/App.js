db5697764ce321db292ba2b9519c656edb36aa6b
Mouse <dino11070808@gmail.com>
2021-06-02 21:07:12 +0800

    [Added by Dino] 整理Alex code webrtc-client-server 至webrtc-camera-web。

--- webrtc-camera-web/src/App.js
+++ webrtc-camera-web/src/App.js
@@ -1,7 +1,36 @@
+import { io } from 'socket.io-client';
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const signalServerUrl = "http://192.168.10.115:3333/";
+const socket = io.connect(signalServerUrl);
+const pcConfig = {
+  'iceServers': [
+    {
+      'urls': 'stun:stun.l.google.com:19302'
+    },
+    {
+      'urls': 'turn:18.191.253.152:3478',
+      'username': 'alex',
+      'credential': 'abcdefg'
+    }
+  ]
+};
 
 function App() {
+  const [isStart, setIsStart] = useState(false);
+  const [peerConnection, setPeerConnection] = useState([]);
+
+  const wait = (timer) => {
+    return new Promise(resolve => {
+      setTimeout(() =>{
+        resolve();
+      }, timer)
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
