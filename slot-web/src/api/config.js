b4851bb80179930f618bce3bf4d9a67921031997
fapjsc <p82szh@gmail.com>
2021-07-08 17:35:00 +0800

    change casino api api

--- slot-web/src/api/config.js
+++ slot-web/src/api/config.js
@@ -2,4 +2,4 @@
 export const apiUrl = 'http://220.135.67.240:8000/';
 
 //=== Casino Api ===//
-export const apiCasinoUrl = 'http://192.168.10.240:8081/';
+export const apiCasinoUrl = 'http://220.135.67.240:8081/';
