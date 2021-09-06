b2c329fc2159151c7e4794ee524446513ec752aa
fapjsc <p82szh@gmail.com>
2021-09-06 13:09:29 +0800

    調整版本號格式

--- slot-web/src/layout/Footer.js
+++ slot-web/src/layout/Footer.js
@@ -52,7 +52,7 @@ const Footer = () => {
           {speed && !testSpeed ? `${speed} MB/s` : 'checking...'}
         </span>
       </IconButton>
-      <p>v F.0.2.0.B.0.1.0</p>
+      <p>v-f.0.2.0-b.0.1.0</p>
     </footer>
   );
 };
